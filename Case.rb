8a7ace817767623e9c42895ccda8f4614272cb21
vchaillou <valentin.chaillou@laposte.net>
2015-02-11 18:50:42 +0100

    Amélioration de Case.rb

--- Case.rb
+++ Case.rb
@@ -2,13 +2,26 @@
 # Case.rb
 # Implémentation de la classe Case
 
+# == Classe Case : 
+#	- connaît on état
 class Case
 	
+	# Variables
+	
+	# * Variable d'instance qui représente l'état de la case
+	# * Peut être ' ' (vide), 'B', (bleu) ou 'R' (rouge)
 	@etat
 
 	# Méthodes
 
+	# * Méthode d'instance qui initialise la +Case+
 	def initialize()
-		etat = ' '
+		@etat = ' '
+	end
+	
+	# * Méthode d'instance qui retourne une chaine de caractères décrivant la +Case+
+	# * +Retourne+ : un +String+ décrivant la +Case+
+	def toString()
+		return etat
 	end
 end
