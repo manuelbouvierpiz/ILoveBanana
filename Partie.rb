4b9b6702457bb986e9752d9d7491c3c3c7c7ad99
ParmenonD <damien.parmenon.etu@univ-lemans.fr>
2015-02-27 12:00:52 +0100

    Ajout d'un début de Partie.rb

---
+++ Partie.rb
@@ -0,0 +1,74 @@
+# PARMENON Damien
+# Case.rb
+# Implementation de la classe Partie
+
+# encoding: utf-8
+
+# La classe Partie est une méthode abstraite mère de PartieMonde et PartieLibre
+class Partie
+
+# On se servira dans cette classe du module baseDeDonnees
+	include baseDeDonnee
+
+# Grille sur laquelle le joueur va jouer
+	@grille
+# Tableau des sauvegardes/hypothèse que le joueur a fait
+	@listeSauvegardes
+# Booleen informant si nous sommes dans un mode "hypothèse"
+	@hypothese
+
+# Initialise les variables d'instances
+	def initialize
+		@listeSauvegardes = Array.[]
+		@hypothese = false
+	end
+
+# Méthode permettant de créer une grille
+	def initGrille()
+
+	end
+
+# A modifier
+	def joue()
+		while(!@grille.estTerminee?())
+
+		end
+	end
+
+
+	def calculerScore()
+		return @grille.getClicMin * ( getDifficulte / ( @grille.getTemps * @grille.getClics * 5 ) ) * ( 1 / ( 1 + getnbAide) )
+	end
+
+	def obtenirRegles()
+		return "Règles\n
+		Il ne peut y avoir trois colonnes adjacentes de la même couleur.\n
+		Les lignes et les colonnes sont constitués du même nombre de cellules rouges que de cellules bleues\n
+		Il ne peut y avoir deux lignes ou deux colonnes identiques\n"
+	end
+
+	def obtenirAide()
+		@grille.obtenirAide()
+	end
+
+	def sauvegarder()
+		@hypothese = true
+		@listeSauvegardes[@sauvegarde.length] = @grille
+	end
+
+	def chargerPreHypo(uneSauvegarde)
+		@grille = @listeSauvegardes[0]
+		@listeSauvegardes = Array.[]
+		@hypothese = false
+	end
+
+	def chargerDerniereSauvegarde()
+		@grille = @listeSauvegardes[@listeSauvegardes.length -1]
+		@listeSauvegardes.pop()
+		if(listeSauvegardes.empty?)
+			@listeSauvegardes = Array.[]
+		end
+	end
+end
+	
+
