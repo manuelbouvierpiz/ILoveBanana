26752bd1d4f520f91997ca43981beb1890f5b3f7
NanzhenK <nanzhen_kuang@hotmail.com>
2015-03-13 15:51:45 +0100

    Update Compte.rb

--- Compte.rb
+++ Compte.rb
@@ -5,3 +5,32 @@
 #======Class Compte=====
 
 class Compte
+    @pseudo
+    @motDePasse
+    @emailAdresse
+    @nom
+    @prenom
+    
+    def Grille.creer (pseudo,motDePasse,emailAdresse)
+      new(pseudo,motDePasse,emailAdresse)
+    end
+    
+    def initialize(pseudo,motDePasse,emailAdresse)
+      @pseudo = pseudo
+      @motDePasse = motDePasse
+      @emailAdresse = emailAdresse
+    end
+    
+    def verifierMotDePasse?(unMotDePasse)
+        if(@motDePasse ==unMotDePasse)
+          return true
+        else 
+          return false
+    end
+    
+    def motDePasseLibre?()
+        return @motDePasse == nil
+    end
+    
+    def login()
+end
