8e649e6603bda83ddd364481d73f92d50277829b
AlexandreMoutel <alexandre.moutel.etu@univ-lemans.fr>
2015-03-13 15:34:59 +0100

    Create Monde.rb

---
+++ Monde.rb
@@ -0,0 +1,55 @@
+# Alexandre MOUTEL
+# Monde.rb
+# Implementation de la classe Monde
+
+# == Classe Monde 
+#	- connait son id
+#	- rendre le monde accessible (debloquer), savoir si il est bloque (estDebloque?), connaitre son nom (nom), connaitre son theme(theme)
+
+class Monde
+
+	# Variables 
+	@idMonde
+	@nom
+	@etat
+	
+	# Méthodes
+
+	def Monde.creer(nom)
+		new(nom)
+	end
+
+	# Méthode d'instance qui initialise le Monde
+	def initialize(nom)
+		@nom = nom
+		@etat = false
+	end
+
+	attr_reader :nom, :theme
+
+	# Méthode d'instance qui permet de donner l'accès à un Monde
+	def debloquer()
+		if(@etat == false)
+			@etat = true
+		end
+	end
+
+	# Méthode d'instance qui permet de savoir si le Monde est accessible
+	def estDebloque?()
+		if(@etat)
+			return true
+		else 
+			return false
+		end
+	end
+
+	# Méthode d'instance qui retourne le nom du monde
+	def nom()
+		return @nom
+	end
+
+	# Méthode d'affichage
+	def to_s()
+		print "Monde: " + @nom + "Accès: " + @etat
+	end
+end
