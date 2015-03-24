c98ee2c30c324b07768e5b81b56f933191202494
ParmenonD <damien.parmenon.etu@univ-lemans.fr>
2015-03-24 14:51:46 +0100

    Classe statistiques

---
+++ Statistiques.rb
@@ -0,0 +1,13 @@
+# PARMENON Damien
+# Statistiques.rb
+# Implementation de la classe Statistiques
+
+# encoding: utf-8
+
+# La classe Statistiques permet d'obtenir toute les statistiques dans la base de données
+class Statistiques
+
+	def Statistiques.new(unCompte)
+	def score(uneTaille)
+		
+end
