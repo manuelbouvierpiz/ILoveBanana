2e65b1582a590a51031866d477a808aac012851c
ParmenonD <damien.parmenon.etu@univ-lemans.fr>
2015-03-24 14:52:56 +0100

    Classe statistiques

--- Statistiques.rb
+++ Statistiques.rb
@@ -6,8 +6,35 @@
 
 # La classe Statistiques permet d'obtenir toute les statistiques dans la base de données
 class Statistiques
+	
+	@compte
 
-	def Statistiques.new(unCompte)
+	def Statistiques.creer(unCompte)
+		new(unCompte)
+	end
+
+	def initialize(unCompte)
+		@compte = unCompte
+	end
+
 	def score(uneTaille)
+		return BaseDeDonnees.getScoreTotal(@compte.pseudo, uneTaille)
+	end
+
+	def tempsTotalJeu()
+		return BaseDeDonnees.getTempsTotal(@compte.pseudo)
+	end
+
+	def scoreDifficulte(uneTaille, uneDifficulte)
+		return BaseDeDonnees.getScoreDifficulte(@compte.pseudo, uneTaille, uneDifficulte)
+	end
+
+	def classement(uneTaille, uneDifficulte)
+		return BaseDeDonnees.getClassement(@compte.pseudo, uneTaille, uneDifficulte)
+	end
+
+	def miseAJour(unePartie)
+		
+	end
 		
 end
