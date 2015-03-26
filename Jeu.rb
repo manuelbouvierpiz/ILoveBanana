c4c54c6a8ff107297ea5f7eba0dab1cec871ed51
Jousse Wilfried <wilfried.jousse@univ-lemans.fr>
2015-03-26 11:48:34 +0100

    Version 1.00

---
+++ Jeu.rb
@@ -0,0 +1,22 @@
+class Jeu
+	@compte
+	@aventure
+	@didactitiel
+	@partie
+
+	private_class_method :new
+
+	def Jeu.creer()
+		new
+	end
+
+	def initialize()
+		@compte = Compte.creer
+		@aventure = Aventure.creer
+		@didactitiel = Didactitiel.creer
+		@partie = PartieLibre.creer
+	end
+
+	
+
+end
