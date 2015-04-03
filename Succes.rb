4fbdb9dd3111e89761f26864cdea6fc4efed7f62
vchaillou <valentin.chaillou@laposte.net>
2015-04-03 11:02:10 +0200

    Constructeur => creer

--- Succes.rb
+++ Succes.rb
@@ -2,14 +2,23 @@
 #class Succes 
 # -represente le liste de succes qui est definit en base de donneese
 class Succes
-    @succes
+    
+    attr_reader :succes
+    
+    # Méthode de classe
+    
+    # * Méthode de classe qui permet de créer un nouvel objet Succes
+    def Succes.creer
+        new
+    end
+    
+    # Méthodes d'instance
     
     # Initialiseur
     def initialize()
         @succes = BaseDeDonnees.getSucces();
     end
-
-    attr_reader :succes
+    
     # Méthode pour obtenir le nombre de succes possible
     def nbSuccesTotal()
         return @succes.length
@@ -18,7 +27,7 @@ class Succes
     # Méthode pour debloquer un succes
     #  - Si le succes est dans le liste 
     def debloquer(unSucces)
-        0.upto(@succes.length) do |i| # i pou index
+        0.upto(@succes.length) do |i| # i pour index
             if(unSucces == @succes[i])
                 @succes[i].debloquer()
             end
