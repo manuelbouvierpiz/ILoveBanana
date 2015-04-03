8f9e0e514f80c7af5d4bdd96be164ae7e0f7ceae
vchaillou <valentin.chaillou@laposte.net>
2015-04-03 09:55:50 +0200

    Prise en charge de Compte.COMPTE
    Assure compatibilité avec le pattern singleton de la classe Compte

--- Options.rb
+++ Options.rb
@@ -6,9 +6,6 @@
 
 class Options
 
-# Variable contenant le pseudo du joueur qui joue actuellement
-	@joueur
-
 # Variable contenant le tableau des raccourcis clavier
 	@tabRaccourciClavier
 
@@ -20,27 +17,26 @@ class Options
 
 # Initialise la variable avec notre base de données
 	def initialize(unCompte)
-		@joueur = unCompte
-		@tabRaccourciClavier = [RaccourciClavier.new(1,"Bleu",@joueur.pseudo),
-								RaccourciClavier.new(2,"Rouge",@joueur.pseudo),
-								RaccourciClavier.new(3,"Pause",@joueur.pseudo),
-								RaccourciClavier.new(4,"Hypothèse",@joueur.pseudo)]
+		@tabRaccourciClavier = [RaccourciClavier.new(1,"Bleu",Compte.COMPTE.pseudo),
+								RaccourciClavier.new(2,"Rouge",Compte.COMPTE.pseudo),
+								RaccourciClavier.new(3,"Pause",Compte.COMPTE.pseudo),
+								RaccourciClavier.new(4,"Hypothèse",Compte.COMPTE.pseudo)]
 	end
 
 # Méthode permettant de modifier le volume des bruitages
 	def modifierVolumeBruitage(unVolume)
-		BaseDeDonnees.setVolumeBruitage(@joueur, unVolume)
+		BaseDeDonnees.setVolumeBruitage(Compte.COMPTE.pseudo, unVolume)
 	end
 
 # Méthode permettant de modifier le volume de la musique
 	def modifierVolumeMusique(unVolume)
-		BaseDeDonnees.setVolumeBruitage(@joueur, unVolume)
+		BaseDeDonnees.setVolumeBruitage(Compte.COMPTE.pseudo, unVolume)
 	end
 
 # Méthode permettant de réinitialiser intégralement les données du profil
 	def resetProfil()
-		BaseDeDonnees.ViderReussir(@pseudo)
-		BaseDeDonnees.viderFinir(@pseudo)
+		BaseDeDonnees.ViderReussir(Compte.COMPTE.pseudo)
+		BaseDeDonnees.viderFinir(Compte.COMPTE.pseudo)
 	end
 
 # Méthode permettant de modifier un raccourci clavier
@@ -62,18 +58,18 @@ class Options
 # Méthode permettant de modifier les couleurs appliquées aux cases
 	def changerCouleur(unOuDeux, uneCouleur)
 		if(unOuDeux == 1)
-			BaseDeDonnees.setCouleurUn(@joueur.pseudo, uneCouleur)
+			BaseDeDonnees.setCouleurUn(Compte.COMPTE.pseudo, uneCouleur)
 		else
-			BaseDeDonnees.setCouleurDeux(@joueur.pseudo, uneCouleur)
+			BaseDeDonnees.setCouleurDeux(Compte.COMPTE.pseudo, uneCouleur)
 		end
 	end
 
 # Méthode retournant soit l'une soit l'autre des deux couleurs choisies par l'utilisateur
 	def couleur(unOuDeux)
 		if(unOuDeux == 1)
-			return BaseDeDonnees.getCouleurUn(@joueur.pseudo)
+			return BaseDeDonnees.getCouleurUn(Compte.COMPTE.pseudo)
 		else
-			return BaseDeDonnees.getCouleurDeux(@joueur.pseudo)
+			return BaseDeDonnees.getCouleurDeux(Compte.COMPTE.pseudo)
 		end
 	end
 end
