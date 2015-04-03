041c6a0521722391140cc02e65d4b40164449173
vchaillou <valentin.chaillou@laposte.net>
2015-04-03 10:07:17 +0200

    Prise en charge de Compte.COMPTE
    Pattern singleton sur la classe Compte

--- Statistiques.rb
+++ Statistiques.rb
@@ -7,32 +7,24 @@
 # La classe Statistiques permet d'obtenir toute les statistiques dans la base de données
 class Statistiques
 
-# Variable permettant de sauvegarder le compte de l'utilisateur actuel car il nous sera utile dans les méthodes
-	@compte
-
 # Constructeur de la classe Statistique, elle est amené à être instanciée une seule fois
-	def Statistiques.creer(unCompte)
-		new(unCompte)
-	end
-
-# Initialisation de notre variable d'instance compte
-	def initialize(unCompte)
-		@compte = unCompte
+	def Statistiques.creer()
+		new()
 	end
 
 # Méthode retournant le score total du joueur selon une taille
 	def score(uneTaille)
-		return BaseDeDonnees.getScoreTotal(@compte.pseudo, uneTaille)
+		return BaseDeDonnees.getScoreTotal(Compte.COMPTE.pseudo, uneTaille)
 	end
 
 # Méthode retournant le temps total que le joueur a passé dans les résolutions de grille
 	def tempsTotalJeu()
-		return BaseDeDonnees.getTempsTotal(@compte.pseudo)
+		return BaseDeDonnees.getTempsTotal(Compte.COMPTE.pseudo)
 	end
 
 # Méthode retournant le score total du joueur selon une taille et une difficulté
 	def scoreDifficulte(uneTaille, uneDifficulte)
-		return BaseDeDonnees.getScoreDifficulte(@compte.pseudo, uneTaille, uneDifficulte)
+		return BaseDeDonnees.getScoreDifficulte(Compte.COMPTE.pseudo, uneTaille, uneDifficulte)
 	end
 
 # Méthode retournant le classement des joueurs en fonction du score total sur des grilles de taille et difficulté données
@@ -42,7 +34,8 @@ class Statistiques
 
 # Méthode premettant de mettre à jour la base de données lorsqu'une partie est terminée
 	def miseAJour(unePartie)
-		setGrilleTerminee(@compte.pseudo, unePartie.temps, unePartie.nbClics, unePartie.grille.idGrille, unePartie.nbHypotheses, unePartie.nbAides, unePartie.calculerScore())
+		setGrilleTerminee(Compte.COMPTE.pseudo, unePartie.temps, unePartie.nbClics, unePartie.grille.idGrille, unePartie.nbHypotheses, unePartie.nbAides, unePartie.calculerScore())
+		return self
 	end
 		
 end
