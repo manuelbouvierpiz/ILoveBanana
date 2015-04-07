62a0734d18d24605990e07550f7a8e38e974ac2c
vchaillou <valentin.chaillou@laposte.net>
2015-04-07 10:06:45 +0200

    Ajout de tableauParties
    tableauParties doit être créé avant de créer le Monde

--- Monde.rb
+++ Monde.rb
@@ -1,52 +1,70 @@
 # Alexandre MOUTEL
+# Modifications par Valentin CHAILLOU
 # Monde.rb
 # Implementation de la classe Monde
 
 # == Classe Monde 
-#	- connait son id
+#	- connait son id, son nom, son état (bloqué/débloqué) et ses niveaux
 #	- rendre le monde accessible (debloquer), savoir si il est bloque (estDebloque?), connaitre son nom (nom), connaitre son theme(theme)
 
 class Monde
 
-	# Variables 
-	@idMonde
-	@nom
+	# Variables d'instance
+	
+	# * Variable d'instance (un entier) qui représente l'id du monde
+	# * Accessible en lecture uniquement
+	attr :idMonde, false
+	
+	# * Variable d'instance (un <b>String</b>) qui représente le nom du monde
+	# * Accessible en lecture uniquement
+	attr :nom, false
+	
+	# * Variable d'instance (un <b>String</b>) qui représente l'état du monde
+	# * Non accessible
 	@etat
 	
-	attr_reader :nom
-	# Méthodes
+	# * Variable d'instance qui représente un tableau de niveaux
+	# * Accessible en lecture uniquement
+	# * Le tableau de <b>PartieMonde</b>s doit être créé pour initialiser le monde
+	attr :tableauParties, false
 
-	def Monde.creer(nom)
-		new(nom)
+	# Méthodes d'instance
+	
+	# * Méthode d'instance qui crée un nouveau Monde
+	def Monde.creer(unIdMonde, unNom)
+		new(unIdMonde, unNom)
 	end
 
 	# Méthode d'instance qui initialise le Monde
-	def initialize(nom)
-		@nom = nom
-		@etat = false
+	def initialize(unIdMonde, unNom, unTableauParties)
+		@idMonde = unIdMonde
+		@nom = unNom
+		@etat = "bloqué"
+		@tableauParties = unTableauParties
 	end
 
 	attr_reader :nom
 
 	# Méthode d'instance qui permet de donner l'accès à un Monde
 	def debloquer()
-		if(@etat == false)
-			@etat = true
+		if @etat == "bloqué"
+			@etat = "débloqué"
 		end
 	end
 
 	# Méthode d'instance qui permet de savoir si le Monde est accessible
 	def estDebloque?()
-		return @etat
+		return @etat == "débloqué"
 	end
 
-	# Méthode d'instance qui retourne le nom du monde
-	def nom()
-		return @nom
-	end
-
-	# Méthode d'affichage
+	# * Méthode d'instance qui retourne une chaine de caractères décrivant le +Monde+
 	def to_s()
-		print "Monde: " + @nom + "Accès: " + @etat
+		unResultat = nom()
+		
+		if !estDebloque?
+			unResultat += " (bloqué)"
+		else
+			
+		return unResultat
 	end
 end
