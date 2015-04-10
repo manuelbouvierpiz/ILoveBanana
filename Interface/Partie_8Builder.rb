ac2f98ee8f87a3545c996dae68b59932e2a515ec
NanzhenK <nanzhen_kuang@hotmail.com>
2015-04-10 16:15:40 +0200

    Update Partie_8Builder.rb

--- Interface/Partie_8Builder.rb
+++ Interface/Partie_8Builder.rb
@@ -6,6 +6,10 @@
 #
 require 'gtk2'
 
+#changer le path unshift pour chercher dans le répertoire père
+$LOAD_PATH.unshift File.expand_path("../../Interface", __FILE__)
+load 'TakuzuBuilder.rb'
+
 class Partie_8Builder < TakuzuBuilder
     def initialize
         super(__FILE__, "Partie")
