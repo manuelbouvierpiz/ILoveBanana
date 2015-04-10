7e31dd92f867e4c1c6ee5b30d391e8e6d833895f
vchaillou <valentin.chaillou@laposte.net>
2015-04-10 14:12:38 +0200

    Essai de factorisation

--- Interface/ConnexionBuilder.rb
+++ Interface/ConnexionBuilder.rb
@@ -13,21 +13,9 @@ load 'option.rb'
 class ConnexionBuilder < TakuzuBuilder
 
     def initialize 
-		super()
         self.add_from_file(__FILE__.sub(".rb",".glade"))
-
-        self['window1'].set_window_position Gtk::Window::POS_CENTER
-        self['window1'].signal_connect('destroy') { Gtk.main_quit }
-        self['window1'].show_all
-		# Creation d'une variable d'instance par composant glade
-		self.objects.each() { |p|
-     		instance_variable_set("@#{p.builder_name}".intern, self[p.builder_name])
-		}
-		
-		self.connect_signals{ |handler| 
-			puts handler
-			method(handler) 
-		}
+        
+		super()
 		
 		self['window1'].set_title("Hello Ruby")
 
