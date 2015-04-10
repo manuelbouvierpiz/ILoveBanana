f725d21d8e0e0521cfca01ccd8540e93a9f01e7e
vchaillou <valentin.chaillou@laposte.net>
2015-04-10 14:21:12 +0200

    Second essai de factorisation

--- Interface/TakuzuBuilder.rb
+++ Interface/TakuzuBuilder.rb
@@ -4,21 +4,23 @@ require 'gtk2'
 
 class TakuzuBuilder < Gtk::Builder
 	
-	def initialize
+	def initialize(unNom, unNomDeFenetre)
 		super()
 	
-		self.add_from_file(__FILE__.sub(".rb",".glade"))
+		self.add_from_file(unNom.sub(".rb",".glade"))
 		self['window1'].set_window_position Gtk::Window::POS_CENTER
 		self['window1'].signal_connect('destroy') { Gtk.main_quit }
 		self['window1'].show_all
 		# Creation d'une variable d'instance par composant glade
 		self.objects.each() { |p|
-		instance_variable_set("@#{p.builder_name}".intern, self[p.builder_name])
+			instance_variable_set("@#{p.builder_name}".intern, self[p.builder_name])
 		}
 		self.connect_signals{ |handler|
-		puts handler
-		method(handler)
+			puts handler
+			method(handler)
 		}
+		
+		self['window1'].set_title(unNomDeFenetre)
 		
 	end
 	
