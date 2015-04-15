1fdb767ef649ff9fdbe37442572b1a4cfcf09a3c
ParmenonD <damien.parmenon.etu@univ-lemans.fr>
2015-04-15 18:45:59 +0200

    Avancement

--- Interface/ClassementBuilder.rb
+++ Interface/ClassementBuilder.rb
@@ -9,6 +9,9 @@ load 'Interface/TakuzuBuilder.rb'
 
 class ClassementBuilder < TakuzuBuilder
 
+	@boolNiveau
+	@boolTaille
+
 	def initialize
 		super(__FILE__, "Classement")
 		
@@ -17,14 +20,18 @@ class ClassementBuilder < TakuzuBuilder
   			iter = @listeNiveaux.append
   			iter[0] = e
 		end
-		@combobox1.model=@listeNiveaux
+		@boxTaille.model=@listeNiveaux
 
 		tabTaille = [6, 8, 10, 12]
 		tabTaille.each_with_index do |e|
   			iter = @listeTaille.append
   			iter[0] = e
 		end
-		@combobox2.model=@listeTaille
+		@boxTaille.model=@listeTaille
+
+		@labelRang.set_visible(false)
+    	@labelPseudo.set_visible(false)
+    	@labelScore.set_visible(false)
     end
 
     def on_button2_clicked
@@ -35,7 +42,26 @@ class ClassementBuilder < TakuzuBuilder
     	ouvrirFenetre(SuccesBuilder.new)
     end
 
-    
+    def on_boxNiveau_changed
+    	@boolNiveau = true
+    	if(@boolTaille == true)
+    		@labelRang.set_visible(true)
+    		@labelPseudo.set_visible(true)
+    		@labelScore.set_visible(true)
+    		# Remplissage a partir de la BDD des listes
+    	end
+    end
+
+    def on_boxTaille_changed
+    	@boolTaille = true
+    	if(@boolNiveau == true)
+    		@labelRang.set_visible(true)
+    		@labelPseudo.set_visible(true)
+    		@labelScore.set_visible(true)
+    		# Remplissage a partir de la BDD des listes
+    	end
+    end
+
     Gtk.init
     ClassementBuilder.new
     Gtk.main
