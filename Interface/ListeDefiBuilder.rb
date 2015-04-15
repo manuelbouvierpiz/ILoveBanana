8097112d0520e76783dd0cff2565b380758c4de0
manuel bouvier <spi3010@ic2s114-13.univ-lemans.fr>
2015-04-15 14:49:15 +0200

    Ajout de l'interface Liste des défis

---
+++ Interface/ListeDefiBuilder.rb
@@ -0,0 +1,13 @@
+# encoding: UTF-8
+
+##
+# Auteur Pierre Jacoboni
+# Version 0.1 : Date : Mon Jul 01 10:17:02 CEST 2013
+#
+require 'gtk2'
+
+class ListeDefiBuilder < TakuzuBuilder
+    def initialize
+        super(__FILE__, "Liste des défis")
+    end
+end
