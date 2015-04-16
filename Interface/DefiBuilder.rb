cd73138571cae4dd5f2ebf3a2e3e25281b663e3c
NanzhenK <nanzhen_kuang@hotmail.com>
2015-04-16 14:42:13 +0200

    Update DefiBuilder.rb

--- Interface/DefiBuilder.rb
+++ Interface/DefiBuilder.rb
@@ -9,15 +9,30 @@ require 'gtk2'
 load 'Interface/TakuzuBuilder.rb'
 load 'Interface/TailleDifficulteBuilder.rb'
 load 'Defi.rb'
+load 'Compte.rb'
 
 class DefiBuilder < TakuzuBuilder
+	
+	attr :grille , false
+	
+	attr :score , false
+	
     def initialize
         super(__FILE__, "Defi")
     end
     
     	def on_validerButton_clicked
-		Defi.creer(labelPseudo,unDestinataire,uneGrille,unScore)
-		#ouvrirFenetre(TailleDifficulteBuilder.new)
+		Defi.creer(Compte.COMPTE.pseudo,labelPseudo,@grille,@score)
+		case @grille.taille
+			when 6
+				ouvrirFenetre(Partie6Builder.creer(PartieLibre.creer(6,@grille.difficulte)))
+			when 8
+				ouvrirFenetre(Partie8Builder.creer(PartieLibre.creer(8,@grille.difficulte)))
+			when 10
+				ouvrirFenetre(Partie10Builder.creer(PartieLibre.creer(10,@grille.difficulte)))
+			when 12
+				ouvrirFenetre(Partie12Builder.creer(PartieLibre.creer(12,@grille.difficulte)))
+		end
 	end
 	
 	#Methode de classe qui fermer le defi
@@ -32,5 +47,10 @@ class DefiBuilder < TakuzuBuilder
 		Gtk.init
 		DefiBuilder.new()
 		Gtk.main
+    	end
+    	
+    	def DefiBuilder.creer(uneGrille,unScore)
+    		@grille = uneGrille
+    		@score = unSocre
     	end
 end
