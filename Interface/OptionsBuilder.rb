093566368daf7d47734f84e1581dcb2d6baf5b4a
NanzhenK <nanzhen_kuang@hotmail.com>
2015-04-17 15:32:36 +0200

    Options.rb

--- Interface/OptionsBuilder.rb
+++ Interface/OptionsBuilder.rb
@@ -1,31 +1,59 @@
 # encoding: UTF-8
 
 ##
-# Auteur Alexandre Moutel
-# Version 0.1 : Date : Mon Jul 01 10:17:02 CEST 2013
+# Version 0.2 : Date : Mon Jul 01 10:17:02 CEST 2013
 #
 require 'gtk2'
+load 'Interface/TakuzuBuilder.rb'
+load 'Interface/MenuPrincipalBuilder.rb'
+load 'Options.rb'
+class OptionsBuilder < TakuzuBuilder
+	
+	attr :pseudo ,false
 
-class OptionsBuilder < Gtk::Builder
+	@adjBruitage
+	@adjMusique
+	
+	def initialize(pseudo) 
 
-	def initialize 
-        super()
-        self.add_from_file(__FILE__.sub(".rb",".glade"))
-
-        self['window1'].set_window_position Gtk::Window::POS_CENTER
-        self['window1'].signal_connect('destroy') { Gtk.main_quit }
-        self['window1'].show_all
-		# Creation d'une variable d'instance par composant glade
-		self.objects.each() { |p|
-     		instance_variable_set("@#{p.builder_name}".intern, self[p.builder_name])
-		}
+		super(__FILE__,"Options")
+	end
+	
+	def on_buttonPrecedent_clicked
 		
-		self.connect_signals{ |handler| 
-			puts handler
-			method(handler) 
-		}
+		BaseDeDonnees.setVolumeBruitage(@pseudo,@adjBruitage.value)
+		BaseDeDonnees.setVolumeMusique(@pseudo,@adjMusique.value)
 		
-		self['window1'].set_title("Hello Options")
+		BaseDeDonnees.setRaccourci(pseudo, 1, @entry1)
+		BaseDeDonnees.setRaccourci(pseudo, 2, @entry2)
+		BaseDeDonnees.setRaccourci(pseudo, 3, @entry3)
 
+		BaseDeDonnees.setCouleurUn(pseudo,@colorbutton1.color)
+		BaseDeDonnees.setCouleurDeux(pseudo,@colorbutton2.color)
+
+		ouvrirFenetre(MenuPrincipalBuilder.new)
+	end
+	
+	
+	def DefiBuilder.lancer(pseudo)
+		Gtk.init
+		OptionsBuilder.new(pseudo)
+		Gtk.main
+    	end
+
+	def OptionsBuilder.ceer(pseudo)
+		@pseudo = pseudo 
+
+		@adjBruitage =  Gtk::Adjustment.new(BaseDeDonnees.getVolumeBruitage(pseudo),0,100,1,1,0)
+		@hscaleBruitage.adjustment = @adjBruitage
+		@adjMusique =  Gtk::Adjustment.new(BaseDeDonnees.getVolumeMusique(pseudo),0,100,1,1,0)
+		@hscaleMusique.adjustment = @adjMusique
+		
+		@entry1 = BaseDeDonnees.getRaccourci(pseudo,1)
+		@entry2 = BaseDeDonnees.getRaccourci(pseudo,2)
+		@entry3 = BaseDeDonnees.getRaccourci(pseudo,3)
+
+		@colorbutton1.color = BaseDeDonnees.getCouleurUn(pseudo)
+		@colorbutton2.color = BaseDeDonnees.getCouleurDeux(pseudo)
 	end
 end
