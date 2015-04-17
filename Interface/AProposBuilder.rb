ea0e47f2bf0a4b8c96444183c286275c4f9543c8
ParmenonD <damien.parmenon.etu@univ-lemans.fr>
2015-04-17 19:27:49 +0200

    Finalisation de APropos

--- Interface/AProposBuilder.rb
+++ Interface/AProposBuilder.rb
@@ -6,4 +6,8 @@ class AProposBuilder < TakuzuBuilder
     def initialize
         super(__FILE__, "A Propos")
     end
+
+    def on_boutonRetour_clicked
+    	ouvrirFenetre(MenuPrincipalBuilder.new)
+    end
 end
