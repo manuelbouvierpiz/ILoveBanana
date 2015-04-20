d2bac5f33d41a3e7d470c3100a01d4d6986194dd
vchaillou <valentin.chaillou@laposte.net>
2015-04-20 09:29:00 +0200

    Ajout de la vérification des gems
    Pour ajouter une gem, il suffit de l'ajouter dans le tableau "desGems"

--- TakuzuLanceur.rb
+++ TakuzuLanceur.rb
@@ -1,6 +1,8 @@
 load 'Aventure.rb'
 load 'BaseDeDonnees.rb'
 load 'Case.rb'
+# Valentin CHAILLOU et Damien PARMENON
+
 load 'ClassesBdd.rb'
 load 'Compte.rb'
 load 'Defi.rb'
@@ -36,5 +38,29 @@ load 'Interface/StatistiquesBuilder.rb'
 load 'Interface/SuccesBuilder.rb'
 load 'Interface/TailleDifficulteBuilder.rb'
 load 'Interface/InscriptionBuilder.rb'
+
+### Test des gems ###
+
+desGems = [['mail', 'mail'], ['active_record', 'activerecord'], ['mysql2', 'mysql2']]
+
+begin
+	require 'gtk2'
+rescue
+	puts("GTK2 n'est pas installé sur votre système")
+end
+
+desGems.each do |unDoublon|
+	begin
+		require unDoublon[0]
+	rescue
+		begin
+			system("gem install " + unDoublon[1])
+			require unDoublon[0]
+		rescue
+			print("La gem " + unDoublon[1] + "est manquante. Veuillez l'installer manuellement")
+		end
+	end
+end
+
 # We are 'go' for launch !
 ConnexionBuilder.lancer
