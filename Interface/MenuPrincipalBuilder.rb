2caee2c225a4bde5e0a57860211c44f4b87801f0
ParmenonD <damien.parmenon.etu@univ-lemans.fr>
2015-04-20 17:33:34 +0200

    Ajout de commentaires

--- Interface/MenuPrincipalBuilder.rb
+++ Interface/MenuPrincipalBuilder.rb
@@ -6,41 +6,52 @@
 #
 require 'gtk2'
 
+# == Classe MenuPrincipalBuilder : 
+#   - Permet à l'utilisateur de pouvoir naviguer dans l'application en permettant l'accés aux différentes fonctionnalités de celle-ci
 class MenuPrincipalBuilder < TakuzuBuilder
 
+	# * Méthode d'instance qui initialise la fenêtre correspondante au +MenuPrincipalBuilder+
 	def initialize 
         super(__FILE__, "Menu Principal")
 	end
 
+	# * Méthode d'instance qui ouvre la fenêtre TailleDifficulte si l'utilisateur clique sur le bouton correspondant afin de lancer une partie rapide
 	def on_partieRapideButton_clicked
 		ouvrirFenetre(TailleDifficulteBuilder.new)
 	end
 
+	# * Méthode d'instance qui ouvre la fenêtre Aventure si l'utilisateur clique sur le bouton correspondant
 	def on_aventureButton_clicked
 		ouvrirFenetre(ChoixMondeBuilder.new)
 	end
 	
+	# * Méthode d'instance qui ouvre la fenêtre correspondant à une partie permettant d'apprendre à jouer
 	def on_didacticielButton_clicked
 		ouvrirFenetre(PartieBuilder.new())
 	end
 	
+	# * Méthode d'instance qui ouvre la fenêtre Options si l'utilisateur clique sur le bouton correspondant
 	def on_optionButton_clicked
 		ouvrirFenetre(OptionsBuilder.new)
 	end
 	
+	# * Méthode d'instance qui ouvre la fenêtre Statistiques si l'utilisateur clique sur le bouton correspondant
 	def on_statistiquesButton_clicked
 		Compte.COMPTE.succes.verifierTousLesSucces()
 		ouvrirFenetre(StatistiquesBuilder.new)
 	end
 	
+	# * Méthode d'instance qui ouvre la fenêtre d'à propos si l'utilisateur clique sur le bouton correspondant
 	def on_aProposButton_clicked
 		ouvrirFenetre(AProposBuilder.new)
 	end
 	
+	# * Méthode d'instance qui ouvre la fenêtre de Connexion si l'utilisateur clique sur le bouton correspondant
 	def on_retourButton_clicked
 		ouvrirFenetre(ConnexionBuilder.new)
 	end
 	
+	# * Méthode d'instance qui permet de fermer intégralement l'application
 	def on_quitterButton_clicked
 		Gtk.main_quit
 	end
