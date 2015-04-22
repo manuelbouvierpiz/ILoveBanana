5bc00d50e6abb4d0b2ebc707662506d190ff13d2
ParmenonD <damien.parmenon.etu@univ-lemans.fr>
2015-04-22 16:57:27 +0200

    Correction des bugs, entièrement fonctionnel pour l'instant

--- Interface/PartieDidacticielBuilder.rb
+++ Interface/PartieDidacticielBuilder.rb
@@ -27,15 +27,14 @@ class PartieDidacticielBuilder < PartieBuilder
 	# * Méthode d'instance qui permet de modifier l'état d'une +Case+
 	# * Est automatiquement appelée par Gtk
 	def	on_bouton_clicked(unX, unY)
-		@fenetreInfo
 		case @etape
 			when 0
-  				@fenetreInfo = Gtk::MessageDialog.new(self['window1'], Gtk::Dialog::DESTROY_WITH_PARENT,
+  				fenetreInfo = Gtk::MessageDialog.new(self['window1'], Gtk::Dialog::DESTROY_WITH_PARENT,
                               Gtk::MessageDialog::INFO,
                               Gtk::MessageDialog::BUTTONS_CLOSE,
-                              "Bienvenue dans le Tutoriel de Takuzu Avengers, le but ici est de vous initier aux règles de ce jeu, la première est: Il ne peut y avoir trois cellules adjacentes de la même couleur. Vous pouvez donc remplir à présent les deux cases en blanc.")
-  				@fenetreInfo.run
-  				@fenetreInfo.destroy
+                              "Bienvenue dans le Tutoriel de Takuzu Avengers, le but ici est de vous initier aux règles de ce jeu, la première est: Il ne peut y avoir trois cellules adjacentes de la même couleur. Vous pouvez donc remplir à présent les deux cases blanches.")
+  				fenetreInfo.run
+  				fenetreInfo.destroy
 				@etape = 1
 				eval("@bouton_#{3}_#{1}.modify_bg(Gtk::STATE_NORMAL, Gdk::Color.parse(\"white\"))")
 				eval("@bouton_#{6}_#{1}.modify_bg(Gtk::STATE_NORMAL, Gdk::Color.parse(\"white\"))")
@@ -43,12 +42,12 @@ class PartieDidacticielBuilder < PartieBuilder
 				if((unX == 2 && unY == 0) || (unX == 5 && unY == 0))
 					super(unX, unY)
 				else
-					@fenetreInfo = Gtk::MessageDialog.new(self['window1'], Gtk::Dialog::DESTROY_WITH_PARENT,
+					fenetreInfo = Gtk::MessageDialog.new(self['window1'], Gtk::Dialog::DESTROY_WITH_PARENT,
                               Gtk::MessageDialog::INFO,
                               Gtk::MessageDialog::BUTTONS_CLOSE,
-                              "Bienvenue dans le Tutoriel de Takuzu Avengers, le but ici est de vous initier aux règles de ce jeu, la première est: Il ne peut y avoir trois cellules adjacentes de la même couleur. Vous pouvez donc remplir à présent les deux cases en blanc.")
-  					@fenetreInfo.run
-  					@fenetreInfo.destroy
+                              "Remplissez les deux cases blanches à l'aide de la première règle qui est : Il ne peut y avoir trois cellules adjacentes de la même couleur. ")
+  					fenetreInfo.run
+  					fenetreInfo.destroy
 					@etape = 1
 					eval("@bouton_#{3}_#{1}.modify_bg(Gtk::STATE_NORMAL, Gdk::Color.parse(\"white\"))")
 					eval("@bouton_#{6}_#{1}.modify_bg(Gtk::STATE_NORMAL, Gdk::Color.parse(\"white\"))")
@@ -56,12 +55,12 @@ class PartieDidacticielBuilder < PartieBuilder
 
 				if(Jeu.JEU.partie.grille.matrice[2][0].estRouge? && Jeu.JEU.partie.grille.matrice[5][0].estRouge?)
 					eval("@bouton_#{1}_#{1}.modify_bg(Gtk::STATE_NORMAL, Gdk::Color.parse(\"white\"))")
-					@fenetreInfo = Gtk::MessageDialog.new(self['window1'], Gtk::Dialog::DESTROY_WITH_PARENT,
+					fenetreInfo = Gtk::MessageDialog.new(self['window1'], Gtk::Dialog::DESTROY_WITH_PARENT,
                               Gtk::MessageDialog::INFO,
                               Gtk::MessageDialog::BUTTONS_CLOSE,
-                              "Félicitations, nous pouvons maintenant passer à la suite, une nouvelle case se colore en blanc, il est possible de déterminer sa couleur à l'aide de la première règle mais également avec la seconde qui est : Les lignes et les colonnes sont constitués du même nombre de cellules des deux couleurs. Vous pouvez donc remplir à présent la case blanche.")
-  					@fenetreInfo.run
-  					@fenetreInfo.destroy
+                              "Félicitations, nous pouvons maintenant passer à la suite, une nouvelle case se colore en blanc, il est possible de déterminer sa couleur à l'aide de la première règle mais également avec la seconde qui est : Les lignes et les colonnes sont constituées du même nombre de cellules des deux couleurs.")
+  					fenetreInfo.run
+  					fenetreInfo.destroy
   					@etape = 2
   				end
   			when 2
@@ -69,19 +68,23 @@ class PartieDidacticielBuilder < PartieBuilder
 					super(unX, unY)
 				else
 					eval("@bouton_#{1}_#{1}.modify_bg(Gtk::STATE_NORMAL, Gdk::Color.parse(\"white\"))")
-					@fenetreInfo.run
-  					@fenetreInfo.destroy
+					fenetreInfo = Gtk::MessageDialog.new(self['window1'], Gtk::Dialog::DESTROY_WITH_PARENT,
+                              Gtk::MessageDialog::INFO,
+                              Gtk::MessageDialog::BUTTONS_CLOSE,
+                              "Remplissez la case blanche, il est possible de déterminer sa couleur à l'aide de la première règle mais également avec la seconde qui est : Les lignes et les colonnes sont constituées du même nombre de cellules des deux couleurs.")
+  					fenetreInfo.run
+  					fenetreInfo.destroy
 				end
 
 				if(Jeu.JEU.partie.grille.matrice[0][0].estBleu?)
 					eval("@bouton_#{3}_#{6}.modify_bg(Gtk::STATE_NORMAL, Gdk::Color.parse(\"white\"))")
 					eval("@bouton_#{6}_#{6}.modify_bg(Gtk::STATE_NORMAL, Gdk::Color.parse(\"white\"))")
-					@fenetreInfo = Gtk::MessageDialog.new(self['window1'], Gtk::Dialog::DESTROY_WITH_PARENT,
+					fenetreInfo = Gtk::MessageDialog.new(self['window1'], Gtk::Dialog::DESTROY_WITH_PARENT,
                               Gtk::MessageDialog::INFO,
                               Gtk::MessageDialog::BUTTONS_CLOSE,
-                              "Et voilà pour les deux premières règles ! Histoire de vérifier si elles sont comprises, remplissez les deux prochaines cases blanches.")
-  					@fenetreInfo.run
-  					@fenetreInfo.destroy
+                              "Et voilà pour les deux premières règles ! Histoire de vérifier si elles sont bien comprises, remplissez les deux prochaines cases blanches.")
+  					fenetreInfo.run
+  					fenetreInfo.destroy
   					@etape = 3
   				end
   			when 3
@@ -90,17 +93,21 @@ class PartieDidacticielBuilder < PartieBuilder
 				else
 					eval("@bouton_#{3}_#{6}.modify_bg(Gtk::STATE_NORMAL, Gdk::Color.parse(\"white\"))")
 					eval("@bouton_#{6}_#{6}.modify_bg(Gtk::STATE_NORMAL, Gdk::Color.parse(\"white\"))")
-  					@fenetreInfo.run
-  					@fenetreInfo.destroy
+					fenetreInfo = Gtk::MessageDialog.new(self['window1'], Gtk::Dialog::DESTROY_WITH_PARENT,
+                              Gtk::MessageDialog::INFO,
+                              Gtk::MessageDialog::BUTTONS_CLOSE,
+                              "Remplissez les deux cases blanches. \n(indice : la première règle)")
+  					fenetreInfo.run
+  					fenetreInfo.destroy
 				end
 
 				if(Jeu.JEU.partie.grille.matrice[2][5].estRouge? && Jeu.JEU.partie.grille.matrice[5][5].estRouge?)
-					@fenetreInfo = Gtk::MessageDialog.new(self['window1'], Gtk::Dialog::DESTROY_WITH_PARENT,
+					fenetreInfo = Gtk::MessageDialog.new(self['window1'], Gtk::Dialog::DESTROY_WITH_PARENT,
                               Gtk::MessageDialog::INFO,
                               Gtk::MessageDialog::BUTTONS_CLOSE,
                               "Rien de plus facile, il est temps de passer à la dernière règle, Il ne peut y avoir deux lignes ou deux colonnes identiques, vous pouvez donc remplir les deux nouvelles cases blanches en observant la première et dernière colonne.")
-  					@fenetreInfo.run
-  					@fenetreInfo.destroy
+  					fenetreInfo.run
+  					fenetreInfo.destroy
   					@etape = 4
   					eval("@bouton_#{1}_#{6}.modify_bg(Gtk::STATE_NORMAL, Gdk::Color.parse(\"white\"))")
 					eval("@bouton_#{2}_#{6}.modify_bg(Gtk::STATE_NORMAL, Gdk::Color.parse(\"white\"))")
@@ -109,19 +116,23 @@ class PartieDidacticielBuilder < PartieBuilder
   				if((unX == 0 && unY == 5) || (unX == 1 && unY == 5))
 					super(unX, unY)
 				else
-					@fenetreInfo.run
-  					@fenetreInfo.destroy
+					fenetreInfo = Gtk::MessageDialog.new(self['window1'], Gtk::Dialog::DESTROY_WITH_PARENT,
+                              Gtk::MessageDialog::INFO,
+                              Gtk::MessageDialog::BUTTONS_CLOSE,
+                              "Remplissez les deux cases blanches à l'aide de la dernière règle : Il ne peut y avoir deux lignes ou deux colonnes identiques")
+  					fenetreInfo.run
+  					fenetreInfo.destroy
   					eval("@bouton_#{1}_#{6}.modify_bg(Gtk::STATE_NORMAL, Gdk::Color.parse(\"white\"))")
 					eval("@bouton_#{2}_#{6}.modify_bg(Gtk::STATE_NORMAL, Gdk::Color.parse(\"white\"))")
 				end
 
 				if(Jeu.JEU.partie.grille.matrice[0][5].estRouge? && Jeu.JEU.partie.grille.matrice[1][5].estBleu?)
-					@fenetreInfo = Gtk::MessageDialog.new(self['window1'], Gtk::Dialog::DESTROY_WITH_PARENT,
+					fenetreInfo = Gtk::MessageDialog.new(self['window1'], Gtk::Dialog::DESTROY_WITH_PARENT,
                               Gtk::MessageDialog::INFO,
                               Gtk::MessageDialog::BUTTONS_CLOSE,
                               "Et voilà, vous avez connaissance des trois règles, il ne vous reste plus qu'à terminer la grille.")
-  					@fenetreInfo.run
-  					@fenetreInfo.destroy
+  					fenetreInfo.run
+  					fenetreInfo.destroy
   					@etape = 5
   				end
   			when 5
