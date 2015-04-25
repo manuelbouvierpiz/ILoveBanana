598f95b9899c0724400928cd9282ae0c738126ee
vchaillou <valentin.chaillou@laposte.net>
2015-04-25 14:54:35 +0200

    Déblocage didacticiel désactivé

--- PartieDidacticiel.rb
+++ PartieDidacticiel.rb
@@ -2,10 +2,18 @@
 
 class PartieDidacticiel < Partie 
 	
-	# * Méthode d'instance qui arrête la +PartieDidacticiel+
+	# * Méthode d'instance qui arrête la *PartieDidacticiel*
 	def arreteToi
 		super()
 		mettreEnPauseChronometre()
+	end
+	
+	# * Méthode d'instance qui "termine" la *PartieDidacticiel*
+	# * Retourne *self*
+	def gagner
+	
+		# On ne met pas à jour la BDD pour le didacticiel (car la grille utilisée fait partie de l'aventure)
+		return self
 	end
 	
 end
