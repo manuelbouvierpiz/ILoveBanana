d27f640d12d393c6a5f2a71824f134f0cf9c2d15
vchaillou <valentin.chaillou@laposte.net>
2015-04-27 14:07:48 +0200

    RDoc

--- PartieMonde.rb
+++ PartieMonde.rb
@@ -5,22 +5,27 @@
 
 # == Classe PartieMonde 
 #	- connait son état
-#	- sait rendre la partie accessible (debloquer), si elle est bloque (estDebloque?), donner le nombre d'étoile(s) en fonction du score(nbEtoile), connaitre le score d'un joueur(scoreDuJoueur), réinitialiser la grille de la partie(initGrille)
+#	- sait rendre la partie accessible (debloquer), si elle est bloquée (estDebloque?), donner le nombre d'étoile(s) en fonction du score(nbEtoile), connaitre le score d'un joueur(scoreDuJoueur), réinitialiser la grille de la partie(initGrille)
 class PartieMonde < Partie
 
-	#Variables
+	# Variables
+	
+	# * Variable d'instance (un booléen) non accessible représentant l'état de la *PartieMonde*
 	@etat
 	
-	#Méthodes d'instance
+	# Méthodes d'instance
 
-	# Méthode d'instance qui permet de donner l'accès à une partie dans un monde
+	# * Méthode d'instance qui permet de donner l'accès à la *PartieMonde*
+	# * Retourne *self*
 	def debloquer()	
 		if(@etat == false)
 			@etat = true
 		end
+		return self
 	end
 
-	# Méthode d'instance qui permet de savoir si la +Partie+ est accessible
+	# * Méthode d'instance qui permet de savoir si la *PartieMonde* est accessible
+	# * Retourne *true* si elle est accessible, sinon *false*
 	def estDebloque?()
 		if(@etat)
 			return true
@@ -29,19 +34,24 @@ class PartieMonde < Partie
 		end
 	end
 
-	# * Méthode d'instance qui retourne un nombre d'étoiles gagnées lors de la +PartieMonde+
+	# * Méthode d'instance qui retourne le nombre d'étoile(s) gagnées pour cette *PartieMonde*
+	# * *ATTENTION* : le nombre d'étoiles est juste retourné, pas calculé (il est calculé dans la méthode gagner)
+	# * Retourne un entier représentant le nombre d'étoile(s) gagnées pour cette *PartieMonde*
 	def nbEtoile()
 		return BaseDeDonnees.getNbEtoileObtenu(Compte.COMPTE.pseudo, @grille.idGrille)
 	end
 	
-	# * Méthode d'instance qui arrête la +PartieDidacticiel+
+	# * Méthode d'instance qui arrête la *PartieMonde*
+	# * Retourne *self*
 	def arreteToi
 		super()
 		mettreEnPauseChronometre()
+		return self
 	end
 
-	# * Méthode d'instance qui "termine" la +Partie+
-	# * Retourne +self+
+	# * Méthode d'instance qui "termine" la *PartieMonde*
+	# * Met à jour la base de données
+	# * Retourne *self*
 	def gagner
 		# On ne met à jour la BDD que si le score est meilleur
 		unScore = calculerScore()
@@ -61,6 +71,8 @@ class PartieMonde < Partie
 		return super()
 	end
 
+	# * Méthode d'instance qui initialise l'état (bloqué/débloqué) de la *PartieMonde*
+	# * Retourne un booléen représentant l'état de la *PartieMonde* (<b>true</b> ou <b>false</b>)
 	def initialiseEtat
 		numNiveau = BaseDeDonnees.getNumeroNiveau(@grille.idGrille)
 		if(@grille.difficulte == 1 && numNiveau == 1)
