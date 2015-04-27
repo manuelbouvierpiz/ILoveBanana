58c7d2ed9f10a5aa33c59188c8d591519d878197
vchaillou <valentin.chaillou@laposte.net>
2015-04-27 13:54:05 +0200

    RDoc

--- PartieDidacticiel.rb
+++ PartieDidacticiel.rb
@@ -1,11 +1,15 @@
+# Damien PARMENON
 
-
+# == Classe PartieDidacticiel :
+#		- est une Partie
+#		- sait s'arrêter
 class PartieDidacticiel < Partie 
 	
 	# * Méthode d'instance qui arrête la *PartieDidacticiel*
+	# * Retourne *self*
 	def arreteToi
-		super()
 		mettreEnPauseChronometre()
+		return super()
 	end
 	
 end
