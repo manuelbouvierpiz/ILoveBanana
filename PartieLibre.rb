5fe536f5ab07c4c7593f072d1803dd878aedeaae
vchaillou <valentin.chaillou@laposte.net>
2015-04-27 14:21:16 +0200

    RDoc

--- PartieLibre.rb
+++ PartieLibre.rb
@@ -4,14 +4,25 @@
 
 # encoding: utf-8
 
-
+# == Classe PartieLibre :
+#		- est une Partie
+#		- sait donner la liste de défis, s'arrêter, gagner
 class PartieLibre < Partie
+	
+	# Méthode de classe
 
+	# * Méthode de classe qui permet de créer une nouvelle *PartieLibre*
+	# * === Attributs :
+	#		- uneTaille : un entier représentant la taille de la *Grille* (ou <b>GrilleHardcore</b>) de la *PartieLibre*
+	#		- uneDifficulte : un entier représentant la difficulte de la *Grille* (ou <b>GrilleHardcore</b>) de la *PartieLibre*
 	def PartieLibre.creer(uneTaille, uneDifficulte)
 		new(uneTaille, uneDifficulte)
 	end
 	
-	def initialize(unArgument, uneDifficulte=nil)
+	# Méthodes d'instance
+	
+	# Méthode d'instance qui initialise la *PartieLibre*
+	def initialize(unArgument, uneDifficulte=nil)	# :nodoc:
 		unIdGrille = -1
 		if uneDifficulte == nil		# unArgument = unIdGrille
 			unIdGrille = unArgument
@@ -20,15 +31,19 @@ class PartieLibre < Partie
 		end
 		super(unIdGrille, uneDifficulte >= 8)
 	end
-
+	
+	# * Méthode d'instance qui retourne la liste des défis du *Compte*
+	# * Retourne un tableau de <b>Defi</b>s associés *Compte*
 	def listeDefis
 		return BaseDeDonnees.getDefis(Compte.COMPTE.pseudo)
 	end
 	
 	# * Méthode d'instance qui arrête la *PartieLibre*
+	# * Retourne *self*
 	def arreteToi
 		super()
 		arretChronometre()
+		return self
 	end
 	
 	# * Méthode d'instance qui "termine" la *PartieLibre*
