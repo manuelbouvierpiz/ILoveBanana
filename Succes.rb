277f41a8df3b25b3bf60a52d244402e08e7f08d8
WilEtu <wilfried.jousse.etu@univ-lemans.fr>
2015-04-29 22:13:12 +0200

    RDoc
    correction et ajout

--- Succes.rb
+++ Succes.rb
@@ -10,21 +10,23 @@ class Succes
     
     # * Variable d'instance qui contient tout les +SuccesIndividuel+
     # * Accessible en lecture uniquement
+    # * Est intialisée lors de la création de *Succes*
     attr_reader :succes
     
     # Méthodes d'instance
     
-    # * Initialise la classe +Succes+ avec tout les +SuccesIndividuel+ contenus dans la base de données
+    # * Initialise la classe *Succes* avec tout les *SuccesIndividuel* contenus dans la base de données
     def initialize()
         @succes = BaseDeDonnees.getSucces();
     end
     
     # * Méthode pour obtenir le nombre de succes existant
+    # * Retourne un entier representant le nombre de succes existant
     def nbSuccesTotal()
         return @succes.length
     end
 
-    # Méthode pour debloquer un succes en faisant appel à la fonction de même nom de la classe +SuccesIndividuel+
+    # * Méthode pour debloquer un succes en faisant appel à la fonction de même nom de la classe *SuccesIndividuel*
     def debloquer(unSucces)
         0.upto(@succes.length) do |i| # i pour index
             if(unSucces == @succes[i])
@@ -33,7 +35,7 @@ class Succes
         end
     end
     
-    # Méthode vérifiant pour chaque +SuccesIndividuel+ s'il est apte a être débloquer et le débloque si oui
+    # * Méthode vérifiant pour chaque *SuccesIndividuel* s'il est apte a être débloquer et le débloque si oui
     def verifierTousLesSucces()
         @succes.each do |unSucces|
             if(!unSucces.estDebloque?)
