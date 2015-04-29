ddd993ebff44120f294939f2442087ac65108585
ronanyzeux <ronan.yzeux.etu@univ-lemans.fr>
2015-04-29 22:30:31 +0200

    RDoc

--- Interface/AProposBuilder.rb
+++ Interface/AProposBuilder.rb
@@ -1,12 +1,15 @@
 # encoding: UTF-8
 
 # Manuel Bouvier
-
+# == Classe +AProposBuilder+ :
+#	- est un +TakuzuBuilder+
 class AProposBuilder < TakuzuBuilder
+    # * Méthode d'instance qui initialise la fenêtre correspondante au +AProposBuilderr+
     def initialize
         super(__FILE__, "A Propos")
     end
-
+    
+    # * Méthode d'instance qui ouvre la fenêtre précédente en l'occurence le Menu dans ce cas
     def on_boutonRetour_clicked
     	ouvrirFenetrePrecedente()
     end
