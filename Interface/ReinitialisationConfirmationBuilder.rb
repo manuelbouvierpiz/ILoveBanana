73e77be0084469946936d49c899b376a73e88ab3
ronanyzeux <ronan.yzeux.etu@univ-lemans.fr>
2015-04-30 00:16:28 +0200

    RDoc

--- Interface/ReinitialisationConfirmationBuilder.rb
+++ Interface/ReinitialisationConfirmationBuilder.rb
@@ -2,15 +2,20 @@
 
 # Valentin CHAILLOU
 
+# == Classe *ReinitialisationConfirmationBuilder* :
+#		est un TakuzuBuilder
 class ReinitialisationConfirmationBuilder < TakuzuBuilder
+    # * Méthode d'instance qui intialise le *ReinitialisationConfirmationBuilder*
     def initialize
         super(__FILE__)
     end
-
+    
+    # * Méthode d'instance qui ouvre la fenêtre des options
     def on_boutonNon_clicked
     	ouvrirFenetre(OptionsBuilder.new)
     end
-
+    
+    # * Méthode d'instance qui réinitialise le compte et ouvre la fenêtre du menu principal
     def on_boutonOui_clicked
 	Compte.COMPTE.reinitialiser
     	ouvrirFenetre(MenuPrincipalBuilder.new)
